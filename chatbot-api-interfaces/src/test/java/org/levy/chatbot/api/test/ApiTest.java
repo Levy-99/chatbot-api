d7b66a2a0fe3ae5c08b36ac7dd1d8143e38d266e
Lovy99 <362809422@qq.com>
2023-03-08 19:44:05 -0800

    test model

--- chatbot-api-interfaces/src/test/java/org/levy/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/org/levy/chatbot/api/test/ApiTest.java
@@ -1,8 +1,37 @@
 package org.levy.chatbot.api.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import javax.swing.text.html.parser.Entity;
+import java.io.IOException;
+
 /**
  * @author Levy
  * @description 单元测试
  */
 public class ApiTest {
+
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/88885882214152/topics?scope=all&count=20");
+
+        get.addHeader("cookie","zsxq_access_token=EF96637F-AAC1-1774-47C5-D1BCF129457A_3715FA4B89CE35C4; zsxqsessionid=75a888e8a1d207d763653d13b0045dc1; abtest_env=product");
+        get.addHeader("Content-Type","application/json;charset=utf8");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
 }
